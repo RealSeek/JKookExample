b21419f8f9365c53b9115ab85152f299c307e1f7
RealSeek <1536266519@qq.com>
2022-11-08 15:21:10 +0800

    Update Main.java

--- src/main/java/snw/jkook/example/Main.java
+++ src/main/java/snw/jkook/example/Main.java
@@ -7,16 +7,17 @@ public class Main extends BasePlugin {
     public void onLoad() {
         // 重写 onLoad 并不是必须的，但是您可以在此阶段做一些初始化工作，比如解压默认的配置文件 (saveDefaultConfig)
         // 三个方法内的日志记录也不是必须的，但是善用日志记录有助于描述 Bot 的运行状态！
-        getLogger().info("PingBot loaded successfully"); // getLogger() 返回的是一个 org.slf4j.Logger 的实例对象，为日志记录器
+        getLogger().info("模板插件加载成功!"); // getLogger() 返回的是一个 org.slf4j.Logger 的实例对象，为日志记录器
     }
 
     @Override
     public void onEnable() {
-        getLogger().info("PingBot enabled successfully");
+        getLogger().info("模板插件启用成功!");
     }
+
 
     @Override
     public void onDisable() {
-        getLogger().info("PingBot stopped");
+        getLogger().info("模板插件以关闭!");
     }
 }
